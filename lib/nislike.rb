3b4a75fe8bf98e3a70148b5b74387e971ff2b608
kousei <kyamad9@gmail.com>
2023-01-03 11:27:47 +0900

    Add tests

--- lib/nislike.rb
+++ lib/nislike.rb
@@ -3,6 +3,15 @@
 require_relative "nislike/version"
 
 module Nislike
-  class Error < StandardError; end
-  # Your code goes here...
+  class UsingNilError < StandardError
+    def initialize(msg="Don't use nil")
+      super
+    end
+  end
+
+  def dont_use_nil(value)
+    raise UsingNilError if value.nil?
+    value
+  end
+  module_function :dont_use_nil
 end
--- spec/nislike_spec.rb
+++ spec/nislike_spec.rb
@@ -5,7 +5,12 @@ RSpec.describe Nislike do
     expect(Nislike::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "raises error" do
+    expect { Nislike.dont_use_nil(nil) }.to raise_error(Nislike::UsingNilError)
+  end
+
+  it "Doesn't raise error" do
+    expect(Nislike.dont_use_nil(11)).to equal(11)
+    expect(Nislike.dont_use_nil('abc')).to equal('abc')
   end
 end
