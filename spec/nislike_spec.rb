5f961e03869beef71f0f9c65ca72c04515c4ce97
kousei <kyamad9@gmail.com>
2023-01-03 11:04:32 +0900

    Initialize

---
+++ spec/nislike_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Nislike do
+  it "has a version number" do
+    expect(Nislike::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
